013cc11237f56870eb9ed4e450a3eeb9a74ea891
lahaina <nattolecats@gmail.com>
2024-11-30 00:20:47 +0900

    bangkk: Fix PRODUCT_MODEL

--- lineage_bangkk.mk
+++ lineage_bangkk.mk
@@ -18,7 +18,7 @@ PRODUCT_NAME := lineage_bangkk
 PRODUCT_DEVICE := bangkk
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g34 5G
+PRODUCT_MODEL := moto g84 5G
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
